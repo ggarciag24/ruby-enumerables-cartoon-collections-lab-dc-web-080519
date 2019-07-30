01a77cbca35f42b0209971a311eafb8d0bc2ce61
German Garcia <ggarciag.business@outlook.com>
2019-07-30 17:23:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,31 +5,31 @@
       puts "#{index + 1} #{item}"
   end
 end
-binding.pry
-def summon_captain_planet(source_array)
-    new = source_array.map {|name| name.capitalize}
-    new.map {|name|  "#{name}!"}
-end
 
-def long_planeteer_calls(source_array)
-    source_array.map {|word| 
-      if word.length < 4 
-        return true
-      else
-        return false
-      end
-    }
-end
+# def summon_captain_planet(source_array)
+#     new = source_array.map {|name| name.capitalize}
+#     new.map {|name|  "#{name}!"}
+# end
 
-def find_the_cheese(source_array)
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    if source_array.include?("cheddar") == true
-      return "cheddar"
-    else if source_array.include?("gouda") == true
-      return "gouda"
-    else if source_array.include?("camembert") == true
-      return "camembert"
-    else 
-      return nil
-    end
-end
+# def long_planeteer_calls(source_array)
+#     source_array.map {|word| 
+#       if word.length < 4 
+#         return true
+#       else
+#         return false
+#       end
+#     }
+# end
+
+# def find_the_cheese(source_array)
+#     cheese_types = ["cheddar", "gouda", "camembert"]
+#     if source_array.include?("cheddar") == true
+#       return "cheddar"
+#     else if source_array.include?("gouda") == true
+#       return "gouda"
+#     else if source_array.include?("camembert") == true
+#       return "camembert"
+#     else 
+#       return nil
+#     end
+# end
