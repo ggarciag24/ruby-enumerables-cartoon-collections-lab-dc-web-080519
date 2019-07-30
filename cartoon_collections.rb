be951c97103b559a4c4d2aa8e34744be3cf6896c
German Garcia <ggarciag.business@outlook.com>
2019-07-30 18:14:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,11 +24,7 @@ end
 def find_the_cheese(source_array)
   
   if source_array.include?("cheddar") == true
-    puts"cheddar"
-    binding.pry
-  else if source_array.include?("gouda") == true
-    return "gouda"
-  else
+    return "cheddar"
     
   end  
 end
